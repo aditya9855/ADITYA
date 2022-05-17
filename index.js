53510a92214bc132097b8db428d0dd6d4f02a8d9
Aditya Singh <62770104+aditya9855@users.noreply.github.com>
2022-05-17 13:29:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
 	container.classList.add("right-panel-active");
@@ -9,3 +6,35 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+function formValidation() { 
+	const name = document.getElementById("name");
+    
+    const password = document.getElementById("password");
+
+	if (name.value.length < 2 || name.value.length > 20) {
+		alert("enter valid name")
+		name.focus();
+		return false;
+	}
+
+	if (!password.value.match(/^.{5,15}$/)) {
+		alert("Password length must be between 5-15 characters!");
+		password.focus();
+		return false;
+	}
+	return true;
+
+}
+function formValidation1() { 
+    
+    let password1 = document.getElementById("password1");
+
+	if (!password1.value.match(/^.{5,15}$/)) {
+		alert("Password length must be between 5-15 characters!");
+		password1.focus();
+		return false;
+	}
+	return true;
+
+}
